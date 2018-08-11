76722ea5f5d04bdf5f93f34488e809f2f0c5fd88
Ivan Podogov <ginkage@yandex.ru>
2018-08-11 16:22:37 +0100

    Remove unused libraries, reformat a bit.

--- app/src/main/java/com/ginkage/ejlookup/Settings.java
+++ app/src/main/java/com/ginkage/ejlookup/Settings.java
@@ -2,12 +2,11 @@ package com.ginkage.ejlookup;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.CheckBoxPreference;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
 import android.view.Gravity;
@@ -16,70 +15,65 @@ import android.widget.TextView;
 import java.util.Locale;
 
 public class Settings extends PreferenceActivity {
-	SharedPreferences.OnSharedPreferenceChangeListener listener;
+    SharedPreferences.OnSharedPreferenceChangeListener listener;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		final String theme = EJLookupActivity.getPrefString(getString(R.string.setting_theme_color), "0");
-		if (theme.equals("1"))
-			setTheme(R.style.AppThemeLight);
-		final String def_lang = Locale.getDefault().getLanguage();
-		final String lang = EJLookupActivity.getPrefString(getString(R.string.setting_language), def_lang);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        final String theme = EJLookupActivity.getPrefString(getString(R.string.setting_theme_color), "0");
+        setTheme(theme.equals("1") ? R.style.AppThemeLight : R.style.AppTheme);
+        final String def_lang = Locale.getDefault().getLanguage();
+        final String lang = EJLookupActivity.getPrefString(getString(R.string.setting_language), def_lang);
 
-		final Context ctx = this;
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-		listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-			public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
-				if (key.equals(getString(R.string.setting_theme_color)) || key.equals(getString(R.string.setting_language))) {
-					String new_theme = EJLookupActivity.getPrefString(getString(R.string.setting_theme_color), "0");
-					String new_lang = EJLookupActivity.getPrefString(getString(R.string.setting_language), def_lang);
-					if ((!new_lang.equals(lang) && !new_lang.equals("0")) || !new_theme.equals(theme)) {
-						final TextView message = new TextView(ctx);
-						message.setText(getString(R.string.restart_msg)); //"");
-						message.setPadding(5, 5, 5, 5);
-						message.setGravity(Gravity.CENTER);
+        final Context ctx = this;
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        listener = (prefs, key) -> {
+            if (key.equals(getString(R.string.setting_theme_color)) || key.equals(getString(R.string.setting_language))) {
+                String new_theme = EJLookupActivity.getPrefString(getString(R.string.setting_theme_color), "0");
+                String new_lang = EJLookupActivity.getPrefString(getString(R.string.setting_language), def_lang);
+                if ((!new_lang.equals(lang) && !new_lang.equals("0")) || !new_theme.equals(theme)) {
+                    final TextView message = new TextView(ctx);
+                    message.setText(getString(R.string.restart_msg)); //"");
+                    message.setPadding(5, 5, 5, 5);
+                    message.setGravity(Gravity.CENTER);
 
-						AlertDialog alertDialog = new AlertDialog.Builder(ctx)
-								.setTitle(getString(R.string.app_name))
-								.setIcon(R.drawable.icon)
-								.setPositiveButton(getString(android.R.string.ok), null)
-								.setView(message)
-								.create();
+                    AlertDialog alertDialog = new AlertDialog.Builder(ctx)
+                            .setTitle(getString(R.string.app_name))
+                            .setIcon(R.drawable.icon)
+                            .setPositiveButton(getString(android.R.string.ok), null)
+                            .setView(message)
+                            .create();
 
-						alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-							public void onDismiss(DialogInterface dialog) {
-								EJLookupActivity.keepMount = true;
-								Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
-								i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-								startActivity(i);
-								System.exit(0);
-							}
-						});
+                    alertDialog.setOnDismissListener(dialog -> {
+                        EJLookupActivity.keepMount = true;
+                        Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(i);
+                        System.exit(0);
+                    });
 
-						alertDialog.show();
-					}
-				}
-			}
-		};
-		preferences.registerOnSharedPreferenceChangeListener(listener);
+                    alertDialog.show();
+                }
+            }
+        };
+        preferences.registerOnSharedPreferenceChangeListener(listener);
 
-		super.onCreate(savedInstanceState);
-		EJLookupActivity.checkPreferences(this);
-		EJLookupActivity.getPrefBoolean(getString(R.string.setting_suggest_romaji), true);
-		addPreferencesFromResource(R.xml.settings);
-		PreferenceCategory listDict = (PreferenceCategory)findPreference(getString(R.string.setting_dictionaries));
+        super.onCreate(savedInstanceState);
+        EJLookupActivity.checkPreferences(this);
+        EJLookupActivity.getPrefBoolean(getString(R.string.setting_suggest_romaji), true);
+        addPreferencesFromResource(R.xml.settings);
+        PreferenceCategory listDict = (PreferenceCategory)findPreference(getString(R.string.setting_dictionaries));
 
-		int i = 0;
-		for (String fileName : DictionaryTraverse.fileList) {
-			boolean exists = DictionaryTraverse.checkExists(fileName) || DictionaryTraverse.bugKitKat;
-			boolean checked = EJLookupActivity.getPrefBoolean(fileName, true);
+        int i = 0;
+        for (String fileName : DictionaryTraverse.fileList) {
+            boolean exists = DictionaryTraverse.checkExists(fileName) || DictionaryTraverse.bugKitKat;
+            boolean checked = EJLookupActivity.getPrefBoolean(fileName, true);
 
-			CheckBoxPreference cb = new CheckBoxPreference(this);
-			cb.setKey(fileName);
-			cb.setTitle(fileName);
-			cb.setSummary(DictionaryTraverse.fileDesc[i++]);
-			cb.setChecked(checked && exists);
-			listDict.addPreference(cb);
-		}
-	}
+            CheckBoxPreference cb = new CheckBoxPreference(this);
+            cb.setKey(fileName);
+            cb.setTitle(fileName);
+            cb.setSummary(DictionaryTraverse.fileDesc[i++]);
+            cb.setChecked(checked && exists);
+            listDict.addPreference(cb);
+        }
+    }
 }
